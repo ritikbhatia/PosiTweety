d44b18eb10b07c009a630bfe013665dc09ab9024
Aishik Nagar <aishiknagar@gmail.com>
2020-11-21 21:19:44 +0800

    Added visualization dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import '../node_modules/react-vis/dist/style.css';
 import FocusGraph from './components/HomePage';
+import Viz from './components/CategoryViz';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <FocusGraph />
+        {/* <FocusGraph /> */}
+        <Viz></Viz>
       </div>
     );
   }
